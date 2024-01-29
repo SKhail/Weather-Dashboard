d3760296142b314a4977eead56a7fbdeedd13e26
Said Khail <saidkhail@hotmail.com>
2024-01-29 13:57:09 +0000

    Adding the local storage to show the users historical data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,37 @@
+//local storage to save up to 5 cities 
+function storeCity(cityName) {
+
+ //get the cities from the local storage / present an empty  array
+ let storeCities = JSON.parse(localStorage.getItem("storeCities")) || ""
+
+ //ensure the city is already in the array
+ if (!storeCities.includes(cityName)) {
+
+  storeCities.push(cityName); // add it in the array 
+ }
+
+ //Max it to 5 cities for storing 
+ if (storeCities.length > 5) {
+  storeCities = storeCities.slice(-5);
+ }
+
+ //Save the updated arr and link it with the local storage
+ localStorage.setItem("storeCities", JSON.stringify(storeCities));
+
+}
+
+//To present the cities from the local storage 
+
+
+
+
 // 5 day forecast
-function fiveDayForecast(cityName) {
+function fiveFetchForecast(cityName) {
 
  const apiKey = "4fd0d648b21702bcd98b75342a9bb88f";
- const fiveDayUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchForCity}&appid=${apiKey}`;
+ const fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchForCity}&appid=${apiKey}`;
+
+
  console.log("API URL", fiveDayUrl);
 
  return fetch(fiveDayUrl)
@@ -11,12 +40,9 @@ function fiveDayForecast(cityName) {
   })
   .catch(function (error) {  // if it isn't passing through this error will appear
    console.error(`Error Occurred when Fetching the Weather ${error}`);
+   throw error;
   });
 }
-
-
-
-
 
 //Searching for a cities weather 
 $("#search-button").on("click", function (event) {
@@ -43,13 +69,16 @@ $("#search-button").on("click", function (event) {
    })
    .then(function (results) {
     showWeather(results);
+
+    return fiveFetchForecast(searchForCity);   //Fetching 5 day forecast data 
    })
-   //handling if an error occurs when fetching 
+   .then(function (fivedayData) {
+    displayFiveForecast(fivedayData);
+   })
    .catch(function (error) {
-    console.error(`Error Occurred when Fetching the Weather ${error}`);
+    console.error(`Error Occurred when Fetching the Weather ${error}`);    //handling if an error occurs when fetching 
    });
- } else {
-  //Display an error if a user has not entered a city 
+ } else {   //Display an error if a user has not entered a city 
   const alertTime = `
    <div class="alert alert-danger alert-dismissible fade show" role="alert">
    <strong>Attention! Please enter a city!</strong> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -83,7 +112,45 @@ function showWeather(results) {
  $(".cityTemperature").eq(0).text(cityTemp.toFixed(2) + "°C");
  $(".cityWindSpeed").eq(0).text(cityWind + "KPH");
  $(".cityHumid").eq(0).text(cityHumidity);
+}
 
+//  Show the five day forecaste for the searched city 
+function displayFiveForecast(fivedayData) {
+
+ const forecastCont = {};
+
+ // empty the data
+ forecastCont.empty();
+
+ fivedayData.list.forEach((results) => {
+  const date = result.dt_txt.split(" ")[0]; // getting the date 
+  console.log(date);
+  if (!forecastCont[date]) {
+   forecastCont[date] = results
+   console.log(forecastCont);
+  }
+
+
+  //combine the forecastCont to show the forecaste on each day 
+
+  Object.values(forecastCont).forEach((results) => {
+   const cityWeatherIcon = results.weather[0].icon;
+   const cityTemp = results.main.temp - 273.15;
+   const cityWind = results.wind.speed;
+   const cityHumidity = results.main.humidity;
+
+   console.log(`Icon: ${cityWeatherIcon}`);
+   console.log(`The cities temperature: ${cityTemp}`);
+   console.log(`The cities wind: ${cityWind}`);
+   console.log(`The cities Humidity: ${cityHumidity}`);
+
+  })
+
+
+
+
+
+ });
 }
 
 
